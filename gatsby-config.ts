a4e71f01b978c8bb65d90d0a368a33b93078162f
David Disch <elijahdisch@gmail.com>
2022-11-15 13:07:16 +1100

    Add trailingSlash always

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,10 +5,14 @@ const config: GatsbyConfig = {
     title: `example-edge-fn`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  flags: {
+    DEV_SSR: true,
+  },
+  trailingSlash: "always",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
+  // graphqlTypegen: true,
   plugins: [],
 }
 
