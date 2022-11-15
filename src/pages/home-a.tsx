1a1d2bbb087f97c140e8cb9f50ef2f0a487da386
David Disch <elijahdisch@gmail.com>
2022-11-15 12:51:34 +1100

    Use static html for rewrites

--- src/pages/home-a.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const HomeA = () => {
-  return <div>Hello, world A</div>
-}
-
-export default HomeA
--- src/pages/home-b.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const HomeB = () => {
-  return <div>Hello, world B</div>
-}
-
-export default HomeB
